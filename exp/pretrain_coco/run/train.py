f8204b54ebe7f3fcbc1f0c7694e491509d64bb5d
BigRedT <tanmay2099@gmail.com>
2019-09-09 16:45:42 -0500

    learning rate

--- exp/pretrain_coco/run/train.py
+++ exp/pretrain_coco/run/train.py
@@ -3,6 +3,9 @@ import click
 
 from utils.constants import Constants, ExpConstants
 from global_constants import coco_paths
+from ..dataset import DetFeatDatasetConstants
+from ..object_encoder import ObjectEncoderConstants
+from ..train import main as train
 
 
 @click.command()
@@ -19,28 +22,41 @@ from global_constants import coco_paths
     default=-1,
     type=int,
     help='Model number. -1 implies begining of training.')
+@click.option(
+    '--lr',
+    default=1e-5,
+    type=float,
+    help='Learning rate')
 def main(**kwargs):
     exp_const = ExpConstants(kwargs['exp_name'],kwargs['exp_base_dir'])
     exp_const.log_dir = os.path.join(exp_const.exp_dir,'logs')
     exp_const.model_dir = os.path.join(exp_const.exp_dir,'models')
     exp_const.vis_dir = os.path.join(exp_const.exp_dir,'vis')
     exp_const.optimizer = 'Adam'
-    exp_const.lr = 1e-3
+    exp_const.lr = kwargs['lr']
     exp_const.momentum = None
     exp_const.num_epochs = 100
     exp_const.log_step = 100
-    exp_const.model_save_step = 1000
+    exp_const.model_save_step = 2000 # 400000/200
     exp_const.val_step = 1000
     exp_const.num_val_samples = None
+    exp_const.batch_size = 200
+    exp_const.num_workers = 10
 
     data_const = {
-        'train': Constants(),
-        'val': Constants()
+        'train': DetFeatDatasetConstants('train'),
+        'val': DetFeatDatasetConstants('val'),
     }
 
     model_const = Constants()
     model_const.model_num = kwargs['model_num']
-    model_const.net = Constants()
-    model_const.net_path = os.path.join(
+    model_const.object_encoder = ObjectEncoderConstants()
+    model_const.object_encoder_path = os.path.join(
         exp_const.model_dir,
-        f'net_{model_const.model_num}')
+        f'object_encoder_{model_const.model_num}')
+
+    train(exp_const,data_const,model_const)
+
+
+if __name__=='__main__':
+    main()
