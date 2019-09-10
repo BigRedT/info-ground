929be6b8b9800faa091d92773172442e5644dc51
BigRedT <tanmay2099@gmail.com>
2019-09-10 11:56:03 -0500

    train.sh update

--- exp/pretrain_coco/scripts/train.sh
+++ exp/pretrain_coco/scripts/train.sh
@@ -1,4 +1,5 @@
 python -m exp.pretrain_coco.run.train \
-    --exp_name 'default_exp' \
-    --model_num 8000 \
-    --lr 1e-4
+    --exp_name 'self_sup_train_batch_size_200' \
+    --model_num -1 \
+    --lr 1e-5 \
+    --train_batch_size 200
