e58f7b822905b4a77174f749860c5ee7d7f7eb53
BigRedT <tanmay2099@gmail.com>
2019-09-14 07:42:02 -0500

    add create_object_index.sh

---
+++ exp/coco_retrieval/scripts/create_object_index.sh
@@ -0,0 +1,3 @@
+python -m exp.coco_retrieval.create_object_index \
+    --labels_hdf5 '/home/tgupta6/Code/context-regions/symlinks/rsaas/coco_proc/detections/val/labels.hdf5' \
+    --object_index_json 'object_index_val.json'
