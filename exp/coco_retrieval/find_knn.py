09f152f175340915670004cc9fb6f88929faf43e
BigRedT <tanmay2099@gmail.com>
2019-09-15 17:48:38 -0500

    vis images

--- exp/coco_retrieval/find_knn.py
+++ exp/coco_retrieval/find_knn.py
@@ -6,8 +6,10 @@ import shutil
 from tqdm import tqdm
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
+import skimage.io as skio
 
 import utils.io as io
+from utils.bbox_utils import vis_bbox
 from utils.html_writer import HtmlWriter
 from detector.model import COCO_INSTANCE_CATEGORY_NAMES
 from global_constants import coco_paths
@@ -35,12 +37,12 @@ def create_search_index(object_index,queries,features_f,search_features_f):
         search_objects_ = []
         search_features_ = []
         object_queries = queries[i]
-        
+        query_image_ids = set([q[0] for q in object_queries])
         object_index_ = random.sample(
             object_index[i],
             min(len(object_index[i]),5000))
         for image_id_obj_id in object_index_:
-            if image_id_obj_id in object_queries:
+            if image_id_obj_id[0] in query_image_ids:
                 continue
                 
             search_objects_.append(image_id_obj_id)
@@ -111,10 +113,13 @@ def get_image_id_to_cap(subset):
     return image_id_to_cap
 
 
-def write_html(knn,vis_dir,image_dir):
-    io.mkdir_if_not_exists(vis_dir)
+
+def write_html(knn,vis_dir,image_dir,boxes_hdf5):
+    io.mkdir_if_not_exists(vis_dir,recursive=True)
     num_objects = len(knn)
-    for i in range(num_objects):
+
+    boxes_f = io.load_h5py_object(boxes_hdf5)
+    for i in tqdm(range(num_objects)):
         obj_name = COCO_INSTANCE_CATEGORY_NAMES[i]
         if obj_name=='N/A':
             continue
@@ -133,18 +138,32 @@ def write_html(knn,vis_dir,image_dir):
             if knn[i][j] is None:
                 continue
 
-            query_image_id = knn[i][j]['query_object'][0]
+            query_image_id, query_box_id = knn[i][j]['query_object']
             src_filename = os.path.join(image_dir,query_image_id+'.jpg')
-            dst_filename = os.path.join(obj_dir,query_image_id+'.jpg')
-            shutil.copyfile(src_filename,dst_filename)
-            col_dict = {0: html_writer.image_tag(query_image_id+'.jpg')}
+            dst_filename = os.path.join(
+                obj_dir,
+                f'{query_image_id}_{query_box_id}.jpg')
+            image = skio.imread(src_filename)
+            bbox = boxes_f[query_image_id][query_box_id]
+            vis_bbox(bbox,image,modify=True)
+            skio.imsave(dst_filename,image)
+            col_dict = {0: html_writer.image_tag(
+                f'{query_image_id}_{query_box_id}.jpg')}
+
             num_retrieved = len(knn[i][j]['retrieved_caps'])
             for k in range(num_retrieved):
-                retrieved_image_id = knn[i][j]['retrieved_objects'][k][0]
+                retrieved_image_id, retrieved_box_id = \
+                    knn[i][j]['retrieved_objects'][k]
                 src_filename = os.path.join(image_dir,retrieved_image_id+'.jpg')
-                dst_filename = os.path.join(obj_dir,retrieved_image_id+'.jpg')
-                shutil.copyfile(src_filename,dst_filename)
-                col_dict[1+k] = html_writer.image_tag(retrieved_image_id+'.jpg')
+                dst_filename = os.path.join(
+                    obj_dir,
+                    f'{retrieved_image_id}_{retrieved_box_id}.jpg')
+                image = skio.imread(src_filename)
+                bbox = boxes_f[retrieved_image_id][retrieved_box_id]
+                vis_bbox(bbox,image,modify=True)
+                skio.imsave(dst_filename,image)
+                col_dict[1+k] = html_writer.image_tag(
+                    f'{retrieved_image_id}_{retrieved_box_id}.jpg')
             
             html_writer.add_element(col_dict)
 
@@ -155,7 +174,16 @@ def write_html(knn,vis_dir,image_dir):
 
             html_writer.add_element(col_dict)
 
+            col_dict = {0: knn[i][j]['query_object']}
+            num_retrieved = len(knn[i][j]['retrieved_objects'])
+            for k in range(num_retrieved):
+                col_dict[1+k] = knn[i][j]['retrieved_objects'][k]
+            
+            html_writer.add_element(col_dict)
+
         html_writer.close()
+    
+    boxes_f.close()
         
 
 @click.command()
@@ -163,6 +191,10 @@ def write_html(knn,vis_dir,image_dir):
     '--object_index_json',
     type=str,
     help='Path to object index json relative to proc_dir')
+@click.option(
+    '--boxes_hdf5',
+    type=str,
+    help='Path to detection boxes hdf5')
 @click.option(
     '--features_hdf5',
     type=str,
@@ -195,86 +227,94 @@ def write_html(knn,vis_dir,image_dir):
     '--vis_dir',
     type=str,
     help='Path to visualization directory where html files would be saved')
+@click.option(
+    '--load_knn_json',
+    is_flag=True,
+    help='Loads an existing knn json file')
 def main(**kwargs):
     random.seed(0)
     os.environ['HDF5_USE_FILE_LOCKING']="FALSE"
-    features_f = io.load_h5py_object(kwargs['features_hdf5'])
-    object_index = io.load_json_object(kwargs['object_index_json'])[:20]
-    num_objects = len(object_index)
 
-    print('Creating image_id to caption map ...')
-    image_id_to_cap = get_image_id_to_cap(kwargs['anno_subset'])
+    if kwargs['load_knn_json'] is not True:
+        features_f = io.load_h5py_object(kwargs['features_hdf5'])
+        object_index = io.load_json_object(kwargs['object_index_json'])[:20]
+        num_objects = len(object_index)
 
-    print('Selecting queries ...')
-    queries = select_queries(object_index,kwargs['num_queries'])
-    query_features = get_query_features(queries,features_f)
+        print('Creating image_id to caption map ...')
+        image_id_to_cap = get_image_id_to_cap(kwargs['anno_subset'])
 
-    print('Creating search index ...')
-    search_features_f = h5py.File(kwargs['search_features_hdf5'],'w')
-    search_object, search_features_f = create_search_index(
-        object_index,
-        queries,
-        features_f,
-        search_features_f)
+        print('Selecting queries ...')
+        queries = select_queries(object_index,kwargs['num_queries'])
+        query_features = get_query_features(queries,features_f)
 
-    print('Find nearest neighbors ...')
-    nbr_ids = find_nn(query_features,search_features_f,kwargs['num_nbrs'])
-    nbrs = [None]*num_objects
-    for i in range(num_objects):
-        nbrs_ = [None]*kwargs['num_queries']
-        search_object_ = search_object[i]
-        nbr_ids_ = nbr_ids[i]
-        num_queries_ = len(nbr_ids_)
-        for j in range(num_queries_):
-            retrieved_nbr_ids = nbr_ids_[j]
-            num_nbrs_ = len(retrieved_nbr_ids)
-            retrieved_nbrs = [None]*num_nbrs_
-            for k in range(num_nbrs_):
-                retrieved_image_id = retrieved_nbr_ids[k]
-                retrieved_nbrs[k] = search_object_[retrieved_image_id]
+        print('Creating search index ...')
+        search_features_f = h5py.File(kwargs['search_features_hdf5'],'w')
+        search_object, search_features_f = create_search_index(
+            object_index,
+            queries,
+            features_f,
+            search_features_f)
+
+        print('Find nearest neighbors ...')
+        nbr_ids = find_nn(query_features,search_features_f,kwargs['num_nbrs'])
+        nbrs = [None]*num_objects
+        for i in range(num_objects):
+            nbrs_ = [None]*kwargs['num_queries']
+            search_object_ = search_object[i]
+            nbr_ids_ = nbr_ids[i]
+            num_queries_ = len(nbr_ids_)
+            for j in range(num_queries_):
+                retrieved_nbr_ids = nbr_ids_[j]
+                num_nbrs_ = len(retrieved_nbr_ids)
+                retrieved_nbrs = [None]*num_nbrs_
+                for k in range(num_nbrs_):
+                    retrieved_image_id = retrieved_nbr_ids[k]
+                    retrieved_nbrs[k] = search_object_[retrieved_image_id]
+                
+                nbrs_[j] = retrieved_nbrs
             
-            nbrs_[j] = retrieved_nbrs
+            nbrs[i] = nbrs_
         
-        nbrs[i] = nbrs_
-    
-    print('Get captions ...')
-    knn = [None]*num_objects
-    for i in range(num_objects):
-        knn_ = [None]*kwargs['num_queries']
-        nbrs_ = nbrs[i]
-        queries_ = queries[i]
-        num_queries_ = len(queries_)
-        for j in range(num_queries_):
-            query_image_id = queries_[j][0]
-            query_cap = image_id_to_cap[query_image_id]
-            retrieved_nbrs = nbrs_[j]
-            num_nbrs_ = len(retrieved_nbrs)
-            retrieved_caps = [None]*num_nbrs_
-            for k in range(num_nbrs_):
-                retrieved_image_id = retrieved_nbrs[k][0]
-                retrieved_caps[k] = image_id_to_cap[retrieved_image_id]
+        print('Get captions ...')
+        knn = [None]*num_objects
+        for i in range(num_objects):
+            knn_ = [None]*kwargs['num_queries']
+            nbrs_ = nbrs[i]
+            queries_ = queries[i]
+            num_queries_ = len(queries_)
+            for j in range(num_queries_):
+                query_image_id = queries_[j][0]
+                query_cap = image_id_to_cap[query_image_id]
+                retrieved_nbrs = nbrs_[j]
+                num_nbrs_ = len(retrieved_nbrs)
+                retrieved_caps = [None]*num_nbrs_
+                for k in range(num_nbrs_):
+                    retrieved_image_id = retrieved_nbrs[k][0]
+                    retrieved_caps[k] = image_id_to_cap[retrieved_image_id]
+                
+                knn_[j] = {
+                    'query_cap': query_cap,
+                    'retrieved_caps': retrieved_caps,
+                    'query_object': queries_[j],
+                    'retrieved_objects': retrieved_nbrs}
             
-            knn_[j] = {
-                'query_cap': query_cap,
-                'retrieved_caps': retrieved_caps,
-                'query_object': queries_[j],
-                'retrieved_objects': retrieved_nbrs}
-        
-        knn[i] = knn_
+            knn[i] = knn_
 
-    io.dump_json_object(knn,kwargs['knn_json'])
+        io.dump_json_object(knn,kwargs['knn_json'])
 
-    features_f.close()
-    search_features_f.close()
+        features_f.close()
+        search_features_f.close()
 
-    #knn = io.load_json_object(kwargs['knn_json'])
+    else:
+        print('Loading existing knn json ...')
+        knn = io.load_json_object(kwargs['knn_json'])
 
     subset = kwargs['anno_subset']
     image_dir = os.path.join(
         coco_paths['image_dir'],
         coco_paths['extracted']['images'][subset])
 
-    write_html(knn,kwargs['vis_dir'],image_dir)
+    write_html(knn,kwargs['vis_dir'],image_dir,kwargs['boxes_hdf5'])
 
 
 if __name__=='__main__':
