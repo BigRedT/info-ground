8919aa16f4eefc15efa13e0dc06d3683974c8cef
BigRedT <tanmay2099@gmail.com>
2019-09-15 17:48:56 -0500

    add scores

--- exp/coco_retrieval/scripts/create_object_index.sh
+++ exp/coco_retrieval/scripts/create_object_index.sh
@@ -1,3 +1,6 @@
+PROC_DIR="/home/tgupta6/Code/context-regions/symlinks/rsaas/coco_proc"
+SUBSET="val"
 python -m exp.coco_retrieval.create_object_index \
-    --labels_hdf5 '/home/tgupta6/Code/context-regions/symlinks/rsaas/coco_proc/detections/val/labels.hdf5' \
-    --object_index_json 'object_index_val.json'
+    --labels_hdf5 "${PROC_DIR}/detections/${SUBSET}/labels.hdf5" \
+    --scores_hdf5 "${PROC_DIR}/detections/${SUBSET}/scores.hdf5" \
+    --object_index_json "${PROC_DIR}/object_index_${SUBSET}.json"
