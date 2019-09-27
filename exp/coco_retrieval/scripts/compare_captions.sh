1f97bde526fea3fc2f42049a7f6166bf85f45835
BigRedT <tanmay2099@gmail.com>
2019-09-27 14:15:14 -0500

    update paths

--- exp/coco_retrieval/scripts/compare_captions.sh
+++ exp/coco_retrieval/scripts/compare_captions.sh
@@ -1,4 +1,4 @@
-EXP_DIR="${PWD}/symlinks/rsaas/coco_exp/self_lang_sup_train_batch_size_200"
+EXP_DIR="${PWD}/symlinks/rsaas/coco_exp/self_lang_sup_train_batch_size_50"
 KNN_JSON="${EXP_DIR}/knn_context_features_val_best.json"
 python -m exp.coco_retrieval.compare_captions \
     --knn_json $KNN_JSON \
--- exp/pretrain_coco/scripts/train_caps.sh
+++ exp/pretrain_coco/scripts/train_caps.sh
@@ -1,5 +1,5 @@
 python -m exp.pretrain_coco.run.train_caps \
-    --exp_name 'self_lang_sup_train_batch_size_200' \
+    --exp_name 'self_lang_sup_train_batch_size_50' \
     --model_num -1 \
     --lr 1e-5 \
     --train_batch_size 50
