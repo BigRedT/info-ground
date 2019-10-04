05487891a6d804c1d903ecb0bf57fdd55b169309
BigRedT <tanmay2099@gmail.com>
2019-10-04 13:41:52 -0500

    path updates in .sh files

--- exp/pretrain_coco/scripts/train_caps.sh
+++ exp/pretrain_coco/scripts/train_caps.sh
@@ -1,5 +1,7 @@
+export HDF5_USE_FILE_LOCKING=FALSE
+
 python -m exp.pretrain_coco.run.train_caps \
-    --exp_name 'self_lang_sup_train_batch_size_50' \
+    --exp_name 'factor_lang_sup_train_batch_size_50' \
     --model_num -1 \
     --lr 1e-5 \
     --train_batch_size 50
