36b4232c5c1a6d2527552529f6f293fdf6083f69
BigRedT <tanmay2099@gmail.com>
2019-10-18 00:04:56 -0500

    add attention vis

--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import skimage.draw as skdraw
 
-def vis_bbox(bbox,img,color=(255,0,0),modify=False):
+def vis_bbox(bbox,img,color=(255,0,0),modify=False,alpha=0.2):
     im_h,im_w = img.shape[0:2]
     x1,y1,x2,y2 = bbox
     x1 = max(0,min(x1,im_w-1))
@@ -20,7 +20,7 @@ def vis_bbox(bbox,img,color=(255,0,0),modify=False):
         color = (color[0],)
 
     rr,cc = skdraw.polygon(r,c,img.shape[:2])
-    skdraw.set_color(img_,(rr,cc),color,alpha=0.2)
+    skdraw.set_color(img_,(rr,cc),color,alpha=alpha)
 
     rr,cc = skdraw.polygon_perimeter(r,c,img.shape[:2])
     
@@ -31,3 +31,17 @@ def vis_bbox(bbox,img,color=(255,0,0),modify=False):
         img_[rr,cc]=color[0]
 
     return img_
+
+
+def create_att(bbox,prev_att,att_value):
+    im_h,im_w = prev_att.shape[0:2]
+    x1,y1,x2,y2 = bbox
+    x1 = int(max(0,min(x1,im_w-1)))
+    x2 = int(max(x1,min(x2,im_w-1)))
+    y1 = int(max(0,min(y1,im_h-1)))
+    y2 = int(max(y1,min(y2,im_h-1)))
+    r = [y1,y1,y2,y2]
+    c = [x1,x2,x2,x1]
+    att = 0*prev_att
+    att[y1:y2,x1:x2] = att_value
+    return np.maximum(prev_att,att)
