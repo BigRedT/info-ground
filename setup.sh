23abd920630b4ebb9effad7ef13d45bada7200bc
BigRedT <tanmay2099@gmail.com>
2019-11-27 17:57:33 -0600

    copy negative features

--- setup.sh
+++ setup.sh
@@ -10,7 +10,10 @@ mkdir $COCO_IMAGES
 # Copy features
 COCO_PROC="${DATA}/coco_proc"
 mkdir $COCO_PROC
-cp -r "${NFS_DATA}/coco_proc/detections" $COCO_PROC
+#cp -r "${NFS_DATA}/coco_proc/detections" $COCO_PROC
 
-
-
+# Copy cache features
+cp -r "${NFS_DATA}/coco_proc/bert_negatives_train2014.h5py" $COCO_PROC
+cp -r "${NFS_DATA}/coco_proc/bert_negatives_val2014.h5py" $COCO_PROC
+cp -r "${NFS_DATA}/coco_proc/bert_noun_negatives_train2014.h5py" $COCO_PROC
+cp -r "${NFS_DATA}/coco_proc/bert_noun_negatives_val2014.h5py" $COCO_PROC
