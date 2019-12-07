bb764ad53d11c15af2a711546b70a4c0c6dbca88
Tanmay Gupta <tanmay2099@gmail.com>
2019-12-07 14:11:46 -0600

    select yaml files based on machine

--- global_constants.py
+++ global_constants.py
@@ -1,14 +1,32 @@
 import yaml
 
+machine='ngc' # {'ngc','htc'}
 
-coco_paths = yaml.load(
-    open('yaml/coco.yml','r'),
-    Loader=yaml.FullLoader)
+if machine=='htc':
+    coco_paths = yaml.load(
+        open('yaml/coco.yml','r'),
+        Loader=yaml.FullLoader)
 
-flickr_paths = yaml.load(
-    open('yaml/flickr.yml','r'),
-    Loader=yaml.FullLoader)
+    flickr_paths = yaml.load(
+        open('yaml/flickr.yml','r'),
+        Loader=yaml.FullLoader)
 
-misc_paths = yaml.load(
-    open('yaml/misc.yml','r'),
-    Loader=yaml.FullLoader)
+    misc_paths = yaml.load(
+        open('yaml/misc.yml','r'),
+        Loader=yaml.FullLoader)
+
+elif machine=='ngc':
+    coco_paths = yaml.load(
+        open('yaml/coco_ngc.yml','r'),
+        Loader=yaml.FullLoader)
+
+    flickr_paths = yaml.load(
+        open('yaml/flickr_ngc.yml','r'),
+        Loader=yaml.FullLoader)
+
+    misc_paths = yaml.load(
+        open('yaml/misc_ngc.yml','r'),
+        Loader=yaml.FullLoader)
+
+else:
+    assert(False),'Machine not implemented'
