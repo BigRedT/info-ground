dbd852468c8e474fa69dbd0e642521162f73df87
Tanmay Gupta <tanmay2099@gmail.com>
2019-12-11 14:01:30 -0600

    update model_num

--- exp/pretrain_coco_negs/scripts/train.sh
+++ exp/pretrain_coco_negs/scripts/train.sh
@@ -2,7 +2,7 @@ export HDF5_USE_FILE_LOCKING=FALSE
 
 python -m exp.pretrain_coco_negs.run.train \
     --exp_name 'bert_negs_lang_loss_1_neg_noun_loss_1_neg_verb_loss_1' \
-    --model_num 48000 \
+    --model_num -1 \
     --lr 1e-5 \
     --train_batch_size 50 \
     --neg_verb_loss_wt 1 \
