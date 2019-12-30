ffeeba9fde2f8f2e900164a7341f9664bd1f3dbf
BigRedT <tanmay2099@gmail.com>
2019-12-30 08:29:03 -0600

    comment copying bert features

--- setup_coco.sh
+++ setup_coco.sh
@@ -5,16 +5,17 @@ mkdir -p $DATA
 # Copy images
 COCO_IMAGES="${DATA}/coco_images"
 mkdir $COCO_IMAGES
-#unzip "${NFS_DATA}/coco_downloads/val2014.zip" -d $COCO_IMAGES
+unzip "${NFS_DATA}/coco_downloads/train2014.zip" -d $COCO_IMAGES
+unzip "${NFS_DATA}/coco_downloads/val2014.zip" -d $COCO_IMAGES
 
 # Copy features
 COCO_PROC="${DATA}/coco_proc"
 mkdir $COCO_PROC
-#cp -r "${NFS_DATA}/coco_proc/detections" $COCO_PROC
+cp -r "${NFS_DATA}/coco_proc/detections" $COCO_PROC
 
-# Copy cache features
-cp -r "${NFS_DATA}/coco_proc/bert_negatives_train2014.h5py" $COCO_PROC
-cp -r "${NFS_DATA}/coco_proc/bert_negatives_val2014.h5py" $COCO_PROC
-cp -r "${NFS_DATA}/coco_proc/bert_noun_negatives_train2014.h5py" $COCO_PROC
-cp -r "${NFS_DATA}/coco_proc/bert_noun_negatives_val2014.h5py" $COCO_PROC
+# # Copy cache features
+# cp -r "${NFS_DATA}/coco_proc/bert_negatives_train2014.h5py" $COCO_PROC
+# cp -r "${NFS_DATA}/coco_proc/bert_negatives_val2014.h5py" $COCO_PROC
+# cp -r "${NFS_DATA}/coco_proc/bert_noun_negatives_train2014.h5py" $COCO_PROC
+# cp -r "${NFS_DATA}/coco_proc/bert_noun_negatives_val2014.h5py" $COCO_PROC
 
