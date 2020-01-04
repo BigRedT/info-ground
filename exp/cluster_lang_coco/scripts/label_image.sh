7e785eca64369cb2181c419bbad24b74adf5ce87
BigRedT <tanmay2099@gmail.com>
2020-01-04 09:43:33 -0600

    update paths

--- exp/cluster_lang_coco/scripts/label_image.sh
+++ exp/cluster_lang_coco/scripts/label_image.sh
@@ -1,4 +1,4 @@
-CLUSTER_DIR="/home/workspace/Data/context-regions/coco_exp/bert_noun_clusters_2000"
+CLUSTER_DIR="/shared/rsaas/tgupta6/Data/context-regions/coco_exp/bert_noun_clusters_500"
 python -m exp.cluster_lang_coco.label_images \
     --feat_info_json "${CLUSTER_DIR}/feat_info.json" \
     --active_words_json "${CLUSTER_DIR}/active_nouns.json" \
