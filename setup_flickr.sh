b6026a77d3d20d73d00e6b6e0549b41c05f498bb
BigRedT <tanmay2099@gmail.com>
2020-01-04 21:35:13 -0600

    uncomment untar

--- setup_flickr.sh
+++ setup_flickr.sh
@@ -2,12 +2,12 @@ NFS_DATA="/shared/rsaas/tgupta6/Data/context-regions"
 DATA="/data/tgupta6/context-regions"
 mkdir -p $DATA
 
-# # Untar images to local data dir
-# FLICKR_IMAGES="${DATA}/flickr30k_images"
-# mkdir $FLICKR_IMAGES
-# tar -xvf "${NFS_DATA}/flickr30k_downloads/flickr30k-images.tar" -C $FLICKR_IMAGES
-# mv $FLICKR_IMAGES/flickr30k-images/* $FLICKR_IMAGES
-# rm -rf $FLICKR_IMAGES/flickr30k-images
+# Untar images to local data dir
+FLICKR_IMAGES="${DATA}/flickr30k_images"
+mkdir $FLICKR_IMAGES
+tar -xvf "${NFS_DATA}/flickr30k_downloads/flickr30k-images.tar" -C $FLICKR_IMAGES
+mv $FLICKR_IMAGES/flickr30k-images/* $FLICKR_IMAGES
+rm -rf $FLICKR_IMAGES/flickr30k-images
 
 # Copy detections to local data dir
 FLICKR_PROC="${DATA}/flickr30k_proc"
