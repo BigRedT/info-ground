500ea958ad19d301f25459708673ad00c676f97c
BigRedT <tanmay2099@gmail.com>
2020-01-23 14:08:51 -0600

    script for flickr cmc feature extraction

--- exp/cmc_flickr/scripts/extract_features.sh
+++ exp/cmc_flickr/scripts/extract_features.sh
@@ -1,6 +1,6 @@
 export HDF5_USE_FILE_LOCKING=FALSE
 SUBSET=$1
-python -m exp.cmc_coco.extract_features \
+python -m exp.cmc_flickr.extract_features \
     --subset $SUBSET \
     --num_workers 20 \
     --batch_size 20
