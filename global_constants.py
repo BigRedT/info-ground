ddc8a4a34d68741c9c9a0cff322066b5de5b0263
BigRedT <tanmay2099@gmail.com>
2020-08-16 18:31:46 -0500

    remove ngc from global_constants

--- global_constants.py
+++ global_constants.py
@@ -1,40 +1,14 @@
 import yaml
 
-machine='htc' # {'ngc','htc'}
 
-if machine=='htc':
-    coco_paths = yaml.load(
-        open('yaml/coco.yml','r'),
-        Loader=yaml.FullLoader)
+coco_paths = yaml.load(
+    open('yaml/coco.yml','r'),
+    Loader=yaml.FullLoader)
 
-    flickr_paths = yaml.load(
-        open('yaml/flickr.yml','r'),
-        Loader=yaml.FullLoader)
+flickr_paths = yaml.load(
+    open('yaml/flickr.yml','r'),
+    Loader=yaml.FullLoader)
 
-    hico_paths = yaml.load(
-        open('yaml/hico.yml','r'),
-        Loader=yaml.FullLoader)
-
-    misc_paths = yaml.load(
-        open('yaml/misc.yml','r'),
-        Loader=yaml.FullLoader)
-
-elif machine=='ngc':
-    coco_paths = yaml.load(
-        open('yaml/coco_ngc.yml','r'),
-        Loader=yaml.FullLoader)
-
-    flickr_paths = yaml.load(
-        open('yaml/flickr_ngc.yml','r'),
-        Loader=yaml.FullLoader)
-
-    hico_paths = yaml.load(
-        open('yaml/hico_ngc.yml','r'),
-        Loader=yaml.FullLoader)
-
-    misc_paths = yaml.load(
-        open('yaml/misc_ngc.yml','r'),
-        Loader=yaml.FullLoader)
-
-else:
-    assert(False),'Machine not implemented'
+misc_paths = yaml.load(
+    open('yaml/misc.yml','r'),
+    Loader=yaml.FullLoader)
