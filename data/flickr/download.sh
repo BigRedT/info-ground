26da50a7ed43607ff506ab59e89c16df835910bb
BigRedT <tanmay2099@gmail.com>
2020-08-17 12:32:17 -0500

    update download.sh

--- data/flickr/download.sh
+++ data/flickr/download.sh
@@ -1,5 +1,5 @@
-DOWNLOAD_DIR="/shared/rsaas/tgupta6/Data/info-ground/flickr30k_downloads"
-git clone git@github.com:BryanPlummer/flickr30k_entities.git $DOWNLOAD_DIR
+download_dir="/shared/rsaas/tgupta6/Data/info-ground/flickr30k_downloads"
+git clone git@github.com:BryanPlummer/flickr30k_entities.git $download_dir
 
-annotations_zip="${DOWNLOAD_DIR}/annotations.zip"
-unzip $annotations_zip -d $DOWNLOAD_DIR
+annotations_zip="${download_dir}/annotations.zip"
+unzip $annotations_zip -d $download_dir
