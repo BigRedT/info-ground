b416d217024257d25f25d39cc753fadc63df643b
BigRedT <tanmay2099@gmail.com>
2020-08-17 07:19:54 -0500

    add EXP_NAME

--- exp/ground/scripts/eval_flickr_phrase_loc.sh
+++ exp/ground/scripts/eval_flickr_phrase_loc.sh
@@ -1,8 +1,9 @@
+EXP_NAME=$1
 export HDF5_USE_FILE_LOCKING=FALSE
 
 LAYERS=2
 python -m exp.ground.run.eval_flickr_phrase_loc \
-    --exp_name "model_trained_on_coco" \
+    --exp_name $EXP_NAME \
     --model_num -100 \
     --no_context \
     --cap_info_nce_layers $LAYERS \
--- exp/ground/scripts/eval_flickr_phrase_loc_model_selection.sh
+++ exp/ground/scripts/eval_flickr_phrase_loc_model_selection.sh
@@ -1,7 +1,8 @@
+EXP_NAME=$1
 export HDF5_USE_FILE_LOCKING=FALSE
 
 python -m exp.ground.run.eval_flickr_phrase_loc_model_selection \
-    --exp_name "model_trained_on_coco" \
+    --exp_name $EXP_NAME \
     --no_context \
     --cap_info_nce_layers 2 \
     --subset val
