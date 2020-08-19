3e530b81ec2e5d44962be7098b9861bb329a1a3a
BigRedT <tanmay2099@gmail.com>
2020-08-19 16:14:09 -0500

    rename DATA to LOCAL_DATA

--- setup_flickr.sh
+++ setup_flickr.sh
@@ -1,16 +1,16 @@
-NFS_DATA="/shared/rsaas/tgupta6/Data/context-regions"
-DATA="/data/tgupta6/context-regions"
-mkdir -p $DATA
+NFS_DATA="/shared/rsaas/tgupta6/Data/info-ground"
+LOCAL_DATA="/data/tgupta6/info-ground"
+mkdir -p $LOCAL_DATA
 
 # Untar images to local data dir
-FLICKR_IMAGES="${DATA}/flickr30k_images"
+FLICKR_IMAGES="${LOCAL_DATA}/flickr30k_images"
 mkdir $FLICKR_IMAGES
 tar -xvf "${NFS_DATA}/flickr30k_downloads/flickr30k-images.tar" -C $FLICKR_IMAGES
 mv $FLICKR_IMAGES/flickr30k-images/* $FLICKR_IMAGES
 rm -rf $FLICKR_IMAGES/flickr30k-images
 
 # Copy detections to local data dir
-FLICKR_PROC="${DATA}/flickr30k_proc"
+FLICKR_PROC="${LOCAL_DATA}/flickr30k_proc"
 mkdir $FLICKR_PROC
 cp -r "${NFS_DATA}/flickr30k_proc/bottomup_flickr_detections" $FLICKR_PROC
 
