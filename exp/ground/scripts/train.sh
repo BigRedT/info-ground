4eb1bf1a13d49d7f7355dff3f33eb05cb081aa2e
BigRedT <tanmay2099@gmail.com>
2020-08-19 21:35:24 -0500

    train.sh supports both datasets

--- exp/ground/scripts/train.sh
+++ exp/ground/scripts/train.sh
@@ -1,7 +1,10 @@
+EXP_NAME=$1
+DATASET=$2
 export HDF5_USE_FILE_LOCKING=FALSE
 
 python -m exp.ground.run.train \
-    --exp_name "model_trained_on_coco" \
+    --exp_name $EXP_NAME \
+    --dataset $DATASET \
     --model_num -1 \
     --neg_noun_loss_wt 1 \
     --self_sup_loss_wt 0 \
